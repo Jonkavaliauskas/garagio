e26ad7483019330b3df92815c136d9a96ee523a3
Nico Burbano <nicobburbano@gmail.com>
2021-03-26 15:02:05 -0400

    Updated routes, added basic pages and routes

---
+++ app/javascript/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div>
+            This is the registration page
+        </div>
+    )
+}
+
+export default Register
--- app/javascript/routes.jsx
+++ app/javascript/routes.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import Home1 from "./components/Home1";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Calendar from "./components/Calendar";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      {/* <Route path="/login" exact component={Login} />
+      <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
-      <Route path="/calendar" exact componant={Calendar} /> */}
-
+      <Route path="/calendar" exact componant={Calendar} />
     </Switch>
   </Router>
 );
