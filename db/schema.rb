17e0d37297a8089eeb9dc60e10a8d52219f846a1
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2021-03-28 15:52:26 +0300

    Add reviews model, run the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_120929) do
+ActiveRecord::Schema.define(version: 2021_03_28_125143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,15 @@ ActiveRecord::Schema.define(version: 2021_03_28_120929) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "phone"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "shop_id"
+    t.integer "customer_id"
+    t.integer "rating"
+    t.text "assessment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "shop_owners", force: :cascade do |t|
