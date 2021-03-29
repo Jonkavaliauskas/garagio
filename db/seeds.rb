5b52c86608a7e82457910f77c340828c0bd1909a
carlviyar <52547511+carlviyar@users.noreply.github.com>
2021-03-29 19:04:07 -0400

    edited packages and seeds slightly to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,6 @@ ShopOwner.destroy_all
 Appointment.destroy_all
 Review.destroy_all
 
-car = Car.create(customer: chad, make: "Toyota", model: "Prius")
-
-# Ingests from CSV to seed vehicle_info db
-csv_text = File.read(Rails.root.join('db', 'lib', 'seeds', 'vehicle_info_seed.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
-    t = VehicleInfo.new
-    t.year = row['year']
-    t.make = row['make']
-    t.model = row['model']
-    t.body_styles = row['body_styles']
-    t.save
-end
 chad = Customer.create(name: "Chad", email: "chad.palmer@yale.edu")
 jonas = Customer.create(name: "Jonas", email: "jonas@yale.edu")
 carl = Customer.create(name: "Carl", email: "carl@yale.edu")
@@ -41,3 +28,18 @@ jeffcars = ShopOwner.create(shop_name: "Jeff Cars", email: "jeff_cars@gmail.com"
 a1 = Appointment.create(shop_owner: carguys, customer: chad, car: prius, date: DateTime.now, car_issue: "ac broke")
 
 r1 = Review.create(shop_owner: carguys, customer: chad, rating: 5, assessment: "fixed issue quickly and correctly!")
+
+
+car = Car.create(customer: chad, make: "Toyota", model: "Prius")
+
+# Ingests from CSV to seed vehicle_info db
+csv_text = File.read(Rails.root.join('db', 'lib', 'seeds', 'vehicle_info_seed.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    t = VehicleInfo.new
+    t.year = row['year']
+    t.make = row['make']
+    t.model = row['model']
+    t.body_styles = row['body_styles']
+    t.save
+end
