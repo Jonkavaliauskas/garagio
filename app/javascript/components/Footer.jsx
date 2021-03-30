ade9681fddbde0d44bb9555d51bbf6c12f07454b
carlviyar <52547511+carlviyar@users.noreply.github.com>
2021-03-30 10:50:34 -0400

    worked on footer a little bit

--- app/javascript/components/Footer.jsx
+++ app/javascript/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className='footer'>
+        <footer className='footer container'>
             <p>&copy; 2021 Garagio</p>
         </footer>
     )
