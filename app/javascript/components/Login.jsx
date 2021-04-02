6f2f6d3a70bf663b7557db4f8d8bea04fe4b2a75
Chad B. Palmer <chad.palmer@yale.edu>
2021-04-02 19:06:47 -0400

    made basic login page with no functionality

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Login = () => {
     return (
-        <div>
-            This is the login page
+        <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+            <div className="jumbotron jumbotron-fluid bg-transparent">
+                <div className="container secondary-color">
+                <h1 className="display-4">Shop Owner Login</h1>
+                <hr className="my-4" />
+                <div className="container">
+                    <form>
+                        <label>
+                            <p>Username</p>
+                            <input type="text"/>
+                        </label>
+                        <label>
+                            <p>Password</p>
+                            <input type="password"/>
+                        </label>
+                        <div>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <Footer />
+                </div>
+            </div>
         </div>
     )
 }
